0230b939404ca07aed545a27b3662d1860d98c0e
Anderson Dadario <anderson@dadario.com.br>
2015-10-07 01:45:25 -0300

    Test.sh -- fixed executable name

--- ruby_scripts/test.sh
+++ ruby_scripts/test.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 cd "/root/install/tests/"
 
-./run_tests.py core/core1.json
-./run_tests.py core/core2.json
-./run_tests.py core/core3.json
-./run_tests.py core/core4.json
-./run_tests.py core/core5.json
-./run_tests.py core/core6.json
+./run_test.py core/core1.json
+./run_test.py core/core2.json
+./run_test.py core/core3.json
+./run_test.py core/core4.json
+./run_test.py core/core5.json
+./run_test.py core/core6.json
 
-./run_tests.py core/createaccount.json
-./run_tests.py core/deposit.json
-./run_tests.py core/withdraw.json
-./run_tests.py core/getbalance.json
+./run_test.py core/createaccount.json
+./run_test.py core/deposit.json
+./run_test.py core/withdraw.json
+./run_test.py core/getbalance.json
 
-./run_tests.py performance/1.json
-./run_tests.py performance/2.json
-./run_tests.py performance/3.json
-./run_tests.py performance/4.json
-./run_tests.py performance/5.json
-./run_tests.py performance/6.json
-./run_tests.py performance/7.json
-./run_tests.py performance/8.json
-./run_tests.py performance/9.json
-./run_tests.py performance/10.json
+./run_test.py performance/1.json
+./run_test.py performance/2.json
+./run_test.py performance/3.json
+./run_test.py performance/4.json
+./run_test.py performance/5.json
+./run_test.py performance/6.json
+./run_test.py performance/7.json
+./run_test.py performance/8.json
+./run_test.py performance/9.json
+./run_test.py performance/10.json
 
-./run_tests.py core/invalid1.json
-./run_tests.py core/timeout1.json
+./run_test.py core/invalid1.json
+./run_test.py core/timeout1.json
