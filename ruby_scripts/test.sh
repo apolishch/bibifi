77ae819bcd67940adb8fca2050ad79816f3df229
Anderson Dadario <anderson@dadario.com.br>
2015-10-07 00:19:45 -0300

    Added test.sh -- bash script to run all tests (make sure to update the directory inside of it before running)

---
+++ ruby_scripts/test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+cd "/root/install/tests/"
+
+./run_tests.py core/core1.json
+./run_tests.py core/core2.json
+./run_tests.py core/core3.json
+./run_tests.py core/core4.json
+./run_tests.py core/core5.json
+./run_tests.py core/core6.json
+
+./run_tests.py core/createaccount.json
+./run_tests.py core/deposit.json
+./run_tests.py core/withdraw.json
+./run_tests.py core/getbalance.json
+
+./run_tests.py performance/1.json
+./run_tests.py performance/2.json
+./run_tests.py performance/3.json
+./run_tests.py performance/4.json
+./run_tests.py performance/5.json
+./run_tests.py performance/6.json
+./run_tests.py performance/7.json
+./run_tests.py performance/8.json
+./run_tests.py performance/9.json
+./run_tests.py performance/10.json
