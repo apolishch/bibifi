482e54a63a3c198c24578f9e234df8136cbf290d
Anderson Dadario <anderson@dadario.com.br>
2015-10-11 18:23:00 -0300

    Makefile

--- ruby_scripts/compile.sh
+++ ruby_scripts/compile.sh
@@ -2,9 +2,10 @@
 
 output_dir="../build"
 
-rm -rf $output_dir/*
+rm -rf $output_dir/atm $output_dir/bank $output_dir/*.card $output_dir/*.auth
 
 cat common.rb validations.rb atm.rb > $output_dir/atm
 cat common.rb validations.rb operations.rb bank.rb > $output_dir/bank
 
-chmod +x $output_dir/*
+chmod +x $output_dir/atm
+chmod +x $output_dir/bank
